6e23b96d92438c97bcda0abe70ef4b86d94358da
valentino-guzmann <venadotuertochatbotvg@gmail.com>
2026-04-24 20:31:24 -0300

    cambio en la request

--- src/main/java/com/chatbotmvt/services/WhatsappService.java
+++ src/main/java/com/chatbotmvt/services/WhatsappService.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
-import java.util.Map;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -37,14 +35,11 @@ public class WhatsappService {
 
         log.info("📤 Enviando WhatsApp -> phone: {}, message: {}", phone, message);
 
-        var request = Map.of(
-                "messaging_product", "whatsapp",
-                "to", phone,
-                "type", "template",
-                "template", Map.of(
-                        "name", "hello_world",
-                        "language", Map.of("code", "en_US")
-                )
+        var request = new SendMessageRequest(
+                "whatsapp",
+                phone,
+                "text",
+                new Text(message)
         );
 
         try {
