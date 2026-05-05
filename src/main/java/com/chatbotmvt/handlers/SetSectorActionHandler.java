06538f16d8c0bc14422d24ecee503fe973828d04
valentino-guzmann <venadotuertochatbotvg@gmail.com>
2026-05-05 16:16:32 -0300

    arreglo

--- src/main/java/com/chatbotmvt/handlers/SetSectorActionHandler.java
+++ src/main/java/com/chatbotmvt/handlers/SetSectorActionHandler.java
@@ -2,25 +2,31 @@ package com.chatbotmvt.handlers;
 
 import com.chatbotmvt.dto.SessionData;
 import com.chatbotmvt.entity.BotFlowRule;
+import com.chatbotmvt.entity.BotOpcion;
 import com.chatbotmvt.entity.UsuarioSesion;
-import com.chatbotmvt.services.SectorService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class SetSectorActionHandler implements BotActionHandler {
 
     @Override
-    public String getActionType() { return "SET_SECTOR"; }
+    public String getActionType() {
+        return "SET_SECTOR";
+    }
 
     @Override
     public String execute(UsuarioSesion sesion, BotFlowRule rule, String input) {
         SessionData data = sesion.getTempData();
-
         Long sectorId = Long.valueOf(rule.getActionValue());
         data.setPendingSectorId(sectorId);
+        return null;
+    }
 
+    @Override
+    public String executeFromOption(UsuarioSesion sesion, BotOpcion opcion, String input) {
+        SessionData data = sesion.getTempData();
+        Long sectorId = Long.valueOf(opcion.getActionValue());
+        data.setPendingSectorId(sectorId);
         return null;
     }
 }
