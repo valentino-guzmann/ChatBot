f019dc119d0f8e01c41ea26aa6c06ca067bc9605
valentino-guzmann <venadotuertochatbotvg@gmail.com>
2026-05-18 12:14:57 -0300

    cambio controller

--- src/main/java/com/chatbotmvt/controller/WebhookController.java
+++ src/main/java/com/chatbotmvt/controller/WebhookController.java
@@ -9,8 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 @Slf4j
